d22860c074ba5bfb3ea1886b98bf4238dbf12e83
tataisei1234-web <tataisei1234@gmail.com>
2026-08-31 09:51:27 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'focus-app-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './icon-192.png',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
